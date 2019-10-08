0ef81a0fc02fee0f0e3befa45f30c98ed8d21ec8
cdomotor <33791546+cdomotor@users.noreply.github.com>
2019-10-08 13:56:48 +1000

    Adding PDFer
    Adding PDFer app to collection

---
+++ AT_Commander_v1/AT_Commander_v1/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace AT_Commander_v1
+{
+    public partial class Form1 : Form
+    {
+        public string eol1;
+        public System.IO.Ports.SerialPort sport;
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadComboBox();
+            cmbEOL1_SelectedIndexChanged(this, new EventArgs());
+        }
+
+        public String mytimestamp()
+        {
+            DateTime dt = DateTime.Now;
+            String thistimestamp = String.Format("{0:HH:mm:ss:ff }", dt);
+            return thistimestamp;
+        }
+
+        void LoadComboBox()
+        {
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            Console.WriteLine("The following serial ports were found:");
+            cmbPort1.Items.Clear();
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+                ComboBox.ObjectCollection items = cmbPort1.Items;
+                items.Add(port);
+            }
+            //Console.ReadLine();
+        }
+
+        private void cboPort1_Click(object sender, EventArgs e)
+        {
+            LoadComboBox();
+        }
+
+        public void serialport_connect(String port, int baudrate, Parity parity, int databits, StopBits stopbits)
+        {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToLongTimeString();
+
+            sport = new System.IO.Ports.SerialPort(
+            port, baudrate, parity, databits, stopbits);
+            sport.ReadTimeout = 100;
+            try
+            {
+                sport.Open();
+                cmdStop1.Enabled = true;
+                cmdGO1.Enabled = false;
+                rtRX1.AppendText("\n" + mytimestamp() + "Connected to " + cmbPort1.Text + "\n");
+                sport.NewLine = eol1;
+                sport.DataReceived += new SerialDataReceivedEventHandler(sport_DataReceived);
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString(), "Error"); }
+        }
+
+        private void sport_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                DateTime dt = DateTime.Now;
+                String dtn = dt.ToLongTimeString();
+                sport.NewLine = eol1;
+                string RXline = sport.ReadLine();
+                SetText(RXline.ToString());
+            }
+            catch (Exception) { }//MessageBox.Show(ex.ToString(), "Error"); }
+        }
+
+        delegate void SetTextCallback(string mytext);
+
+        private void SetText(string mytext)
+        {
+            if (this.rtRX1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { mytext });
+            }
+            else
+            {
+                rtRX1.SelectionColor = Color.LimeGreen;
+                string thistimestamp = mytimestamp();
+                String myrecieved = thistimestamp + "RX: " + mytext + "\n";
+                this.rtRX1.AppendText(myrecieved);
+                //LogToFile(txtLogFilePath.Text + txtLogFileName.Text, myrecieved + txtNote1.Text);
+                
+            }
+        }
+
+        private void cmdConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String port = cmbPort1.Text;
+                int baudrate = Convert.ToInt32(cmbBaud1.Text);
+                Parity parity = (Parity)Enum.Parse(typeof(Parity), cmbParity1.Text);
+                int databits = Convert.ToInt32(cmbDatabits1.Text);
+                StopBits stopbits = (StopBits)Enum.Parse(typeof(StopBits), cmbStopbits1.Text);
+                serialport_connect(port, baudrate, parity, databits, stopbits);
+                //this.ActiveControl = txtTX1;
+                cmdGO1.BackColor = Color.Green;
+            }
+            catch (Exception ex) { MessageBox.Show(ex.ToString(), "Error"); }
+        }
+        
+        private void cmbEOL1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmbEOL1.SelectedIndex + 1)
+            {
+                case 1:
+                    eol1 = "\n";
+                    break;
+                case 2:
+                    eol1 = "\r";
+                    break;
+                case 3:
+                    eol1 = "\n\r";
+                    break;
+                default:
+                    eol1 = "\n";
+                    break;
+            }
+            lblEOL1.Text = cmbEOL1.Text;
+        }
+
+
+
+
+    }
+}
---
+++ PDFer/PDFer/Form1.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.Office.Interop.Word;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+//add a reference to Microsoft.Office.Interop.Word 
+
+namespace PDFer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdDir_Click(object sender, EventArgs e)
+        {
+        //    using (var fbd = new FolderBrowserDialog())
+        //    {
+        //        //DialogResult result = fbd.ShowDialog();
+
+        //        //var dlg1 = new Ionic.Utils.FolderBrowserDialogEx();
+        //        //dlg1.Description = "Select a folder to extract to:";
+        //        //dlg1.ShowNewFolderButton = true;
+        //        //dlg1.ShowEditBox = true;
+        //        ////dlg1.NewStyle = false;
+        //        //dlg1.SelectedPath = "";//txtExtractDirectory.Text;
+        //        //dlg1.ShowFullPathInEditBox = true;
+        //        //dlg1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+
+        //        //DialogResult result = null;
+        //        //CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+        //        ////dialog.InitialDirectory = txtFolder.Text;
+        //        //dialog.IsFolderPicker = true;
+        //        //if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+        //        //{
+        //        //    result = dialog.FileName;
+        //        //}
+
+
+        //        //// Show the FolderBrowserDialog.
+        //        //DialogResult result = dialog.ShowDialog();
+        //        ////if (result == DialogResult.OK)
+        //        ////{
+        //        ////    txtExtractDirectory.Text = dlg1.SelectedPath;
+        //        ////}
+
+        //        if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+        //        {
+        //            //string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+        //            //System.Windows.Forms.MessageBox.Show("Files found: " + files.Length.ToString(), "Message");
+
+        //            // Create a new Microsoft Word application object
+        //            Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+
+        //            // C# doesn't have optional arguments so we'll need a dummy value
+        //            object oMissing = System.Reflection.Missing.Value;
+
+        //            // Get list of Word files in specified directory
+        //            //DirectoryInfo dirInfo = new DirectoryInfo(@"\\server\folder"); fbd.SelectedPath
+        //            DirectoryInfo dirInfo = new DirectoryInfo(fbd.SelectedPath); 
+        //            FileInfo[] wordFiles = dirInfo.GetFiles("*.docx");
+
+        //            word.Visible = false;
+        //            word.ScreenUpdating = false;
+
+        //            foreach (FileInfo wordFile in wordFiles)
+        //            {
+        //                // Cast as Object for word Open method
+        //                Object filename = (Object)wordFile.FullName;
+
+        //                // Use the dummy value as a placeholder for optional arguments
+        //                Document doc = word.Documents.Open(ref filename, ref oMissing,
+        //                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+        //                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+        //                    ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+        //                doc.Activate();
+
+        //                object outputFileName = wordFile.FullName.Replace(".doc", ".pdf");
+        //                object fileFormat = WdSaveFormat.wdFormatPDF;
+
+        //                // Save document into PDF Format
+        //                doc.SaveAs(ref outputFileName,
+        //                    ref fileFormat, ref oMissing, ref oMissing,
+        //                    ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+        //                    ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+        //                    ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+
+        //                // Close the Word document, but leave the Word application open.
+        //                // doc has to be cast to type _Document so that it will find the
+        //                // correct Close method.                
+        //                object saveChanges = WdSaveOptions.wdDoNotSaveChanges;
+        //                ((_Document)doc).Close(ref saveChanges, ref oMissing, ref oMissing);
+        //                doc = null;
+        //            }
+
+        //            // word has to be cast to type _Application so that it will find
+        //            // the correct Quit method.
+        //            ((_Application)word).Quit(ref oMissing, ref oMissing, ref oMissing);
+        //            word = null;
+
+
+        //        }
+        //    }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            
+                        // Create a new Microsoft Word application object
+                        Microsoft.Office.Interop.Word.Application word = new Microsoft.Office.Interop.Word.Application();
+
+                        // C# doesn't have optional arguments so we'll need a dummy value
+                        object oMissing = System.Reflection.Missing.Value;
+
+                        // Get list of Word files in specified directory
+                        DirectoryInfo dirInfo = new DirectoryInfo(txtFolder.Text);
+                        FileInfo[] wordFiles = dirInfo.GetFiles("*.doc*");
+
+                        word.Visible = false;
+                        word.ScreenUpdating = false;
+
+                        foreach (FileInfo wordFile in wordFiles)
+                        {
+                            //object outputFileName = wordFile.FullName.Replace(".doc", ".pdf");
+                            string extension = wordFile.FullName.Substring(wordFile.FullName.IndexOf("."));
+                            object outputFileName = wordFile.FullName.Replace(extension, ".pdf");
+
+                            if (!File.Exists(outputFileName.ToString()))
+                            {
+                                richTextBox1.AppendText("\nConverting file: " + outputFileName.ToString());
+
+                                // Cast as Object for word Open method
+                                Object filename = (Object)wordFile.FullName;
+
+                                // Use the dummy value as a placeholder for optional arguments
+                                Document doc = word.Documents.Open(ref filename, ref oMissing,
+                                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                                    ref oMissing, ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                                    ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+                                doc.Activate();
+
+                                object fileFormat = WdSaveFormat.wdFormatPDF;
+
+                                // Save document into PDF Format
+                                doc.SaveAs(ref outputFileName,
+                                    ref fileFormat, ref oMissing, ref oMissing,
+                                    ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                                    ref oMissing, ref oMissing, ref oMissing, ref oMissing,
+                                    ref oMissing, ref oMissing, ref oMissing, ref oMissing);
+
+                                // Close the Word document, but leave the Word application open.
+                                // doc has to be cast to type _Document so that it will find the
+                                // correct Close method.                
+                                object saveChanges = WdSaveOptions.wdDoNotSaveChanges;
+                                ((_Document)doc).Close(ref saveChanges, ref oMissing, ref oMissing);
+                                doc = null;
+
+                                // word has to be cast to type _Application so that it will find
+                                // the correct Quit method.
+                                ((_Application)word).Quit(ref oMissing, ref oMissing, ref oMissing);
+                                word = null;
+                            }
+                            else
+                            {
+                                richTextBox1.AppendText("\nFile exists: " + outputFileName.ToString());
+                            }
+                
+                        }
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.AppendText("\n\nERROR: " + ex + "\n");
+                //throw;
+            }
+        }
+    }
+}
